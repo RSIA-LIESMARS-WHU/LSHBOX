8774fb8b0cdd5c439553cbc74ad890202e3d1971
primetang <tanggefu@gmail.com>
2014-08-31 19:49:26 +0800

    update

--- python/boostpy_ms.cpp
+++ python/boostpy_ms.cpp
@@ -1,3 +1,25 @@
+//////////////////////////////////////////////////////////////////////////////
+/// Copyright (C) 2014 Gefu Tang <tanggefu@gmail.com>. All Rights Reserved.
+///
+/// This file is part of LSHBOX.
+///
+/// LSHBOX is free software: you can redistribute it and/or modify it under
+/// the terms of the GNU General Public License as published by the Free
+/// Software Foundation, either version 3 of the License, or(at your option)
+/// any later version.
+///
+/// LSHBOX is distributed in the hope that it will be useful, but WITHOUT
+/// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+/// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+/// more details.
+///
+/// You should have received a copy of the GNU General Public License along
+/// with LSHBOX. If not, see <http://www.gnu.org/licenses/>.
+///
+/// @version 0.1
+/// @author Gefu Tang & Zhifeng Xiao
+/// @date 2014.6.30
+//////////////////////////////////////////////////////////////////////////////
 #define MS_WIN64
 #define BOOST_PYTHON_SOURCE
 #include <libs/python/src/numeric.cpp>
